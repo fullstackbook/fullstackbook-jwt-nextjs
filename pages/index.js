778452fb74c4d44e7823e19d55b1411efdc0bf2d
Travis Luong <travis@travisluong.com>
2022-08-28 13:40:27 -0700

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 
 import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
